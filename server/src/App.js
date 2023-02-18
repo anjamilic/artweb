1155c87d2d8119419ceffdb1c5e38ce4a61d8133
mb1112 <bogicmiljana@gmail.com>
2023-02-18 19:48:40 +0100

    Dodavanje app.js-a

---
+++ server/src/App.js
@@ -0,0 +1,45 @@
+// react
+import { useEffect } from 'react';
+
+// react router dom
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+
+// scenes
+import Home from './scenes/home/Home';
+import ItemDetails from './scenes/itemDetails/ItemDetails';
+import Checkout from './scenes/checkout/Checkout';
+import Confirmation from './scenes/checkout/Confirmation';
+import Navbar from './scenes/global/Navbar';
+import CartMenu from './scenes/global/CartMenu';
+import Footer from './scenes/global/Footer';
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+function App() {
+  return (
+    <div className='app'>
+      <BrowserRouter>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/item/:itemId' element={<ItemDetails />} />
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/checkout/success' element={<Confirmation />} />
+        </Routes>
+        <CartMenu />
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
